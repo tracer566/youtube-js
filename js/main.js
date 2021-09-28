1edefc09d425aa98cee8ad9165afb27baa33d86f
Aleksey <pavlyutenkovaleksey@yandex.ru>
2021-09-29 02:06:26 +0500

    Оживил кнопку поиск на мобильной версии

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const searchBtn = document.querySelector('#mobile-search')
+const mobileSearch = document.querySelector('.input-group')
+const btn = document.querySelector('.button-search')
+const logo = document.querySelector('.logo')
+searchBtn.addEventListener('click',() => {
+  mobileSearch.classList.add('is-open')
+  searchBtn.style.display = 'none'
+  logo.style.display = 'none'
+})
+
+btn.addEventListener('click',function(){
+  mobileSearch.classList.remove('is-open')
+  searchBtn.style.display = 'inline-block'
+  logo.style.display = 'block'
+})
+
