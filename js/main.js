17d488437dad4c47bf442a0ec0fa0b68536e69ba
Aleksey <pavlyutenkovaleksey@yandex.ru>
2021-09-29 02:43:20 +0500

    Сжал все картинки

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 if(document.documentElement.clientWidth < 440){
   const searchBtn = document.querySelector('#mobile-search')
-const mobileSearch = document.querySelector('.input-group')
-const btn = document.querySelector('.button-search')
-const logo = document.querySelector('.logo')
-searchBtn.addEventListener('click',() => {
-  mobileSearch.classList.add('is-open')
-  searchBtn.style.display = 'none'
-  logo.style.display = 'none'
-})
+  const mobileSearch = document.querySelector('.input-group')
+  const btn = document.querySelector('.button-search')
+  const logo = document.querySelector('.logo')
+  searchBtn.addEventListener('click',() => {
+    mobileSearch.classList.add('is-open')
+    searchBtn.style.display = 'none'
+    logo.style.display = 'none'
+  })
 
-btn.addEventListener('click',function(){
-  mobileSearch.classList.remove('is-open')
-  searchBtn.style.display = 'inline-block'
-  logo.style.display = 'block'
-})
+  btn.addEventListener('click',function(){
+    mobileSearch.classList.remove('is-open')
+    searchBtn.style.display = 'inline-block'
+    logo.style.display = 'block'
+  })
 }
 
 
